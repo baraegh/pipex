860a3cce7f8783f0446b835fbdf5ea217aa282f9
Elbarae El-ghannouchi <barae.gh@gmail.com>
2022-03-19 01:07:02 +0100

    add utils functions

--- utils.c
+++ utils.c
@@ -6,31 +6,60 @@
 /*   By: eel-ghan <eel-ghan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 23:39:41 by eel-ghan          #+#    #+#             */
-/*   Updated: 2022/03/16 22:29:44 by eel-ghan         ###   ########.fr       */
+/*   Updated: 2022/03/19 01:00:19 by eel-ghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-void	terminate(char *msg)
+char	**get_cmd(char *path, char *command, char **paths)
 {
-	ft_putstr_fd(msg, 2);
-	exit(0);
+	char	**cmd;
+	char	*str;
+
+	str = ft_strjoin(path, "/");
+	if (!str)
+	{
+		free_paths(paths);
+		terminate(ERR_JOIN1);
+	}
+	str = ft_strjoin(str, command);
+	if (!str)
+	{
+		free(str);
+		free_paths(paths);
+		terminate(ERR_JOIN2);
+	}
+	cmd = ft_split(str, ' ');
+	if (!cmd)
+	{
+		free(str);
+		free_paths(paths);
+		terminate(ERR_SPLIT);
+	}
+	return (cmd);
 }
 
-char	*get_cmd(char *path, char *arg)
+void	execute_cmd(char **paths, char *command, char **env)
 {
-	char	*cmd;
-	char	**args;
+	int		i;
+	char	**cmd;
+	char	*msg;
 
-	cmd = ft_strjoin(path, "/");
-	// if (!cmd)
-	// 	return
-	args = ft_split(arg, ' ');
-	// if (!arg)
-	// 	return
-	cmd = ft_strjoin(cmd, args[0]);
-	// if (!cmd)
-	// 	return
-	return (cmd);
+	i = 0;
+	while (paths[i])
+	{
+		cmd = get_cmd(paths[i], command, paths);
+		if (access(cmd[0], X_OK) != -1)
+		{
+			free_paths(paths);
+			execve(cmd[0], cmd, env);
+		}
+		free(cmd);
+		i++;
+	}
+	msg = ft_strjoin("command not found: ", command);
+	if (!msg)
+		ft_putstr_fd("command not found", 2);
+	ft_putstr_fd(msg, 2);
 }
